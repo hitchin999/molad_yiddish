c0adfdf71614f519081e17e1060d0589089e8896
hitchin999 <hitchin999@gmail.com>
2025-05-16 15:59:23 -0400

    Update __init__.py

--- custom_components/molad_yiddish/__init__.py
+++ custom_components/molad_yiddish/__init__.py
@@ -3,14 +3,13 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 
-DOMAIN = "molad_yiddish"
+from .const import DOMAIN
+
 PLATFORMS = [Platform.SENSOR]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Set up Molad Yiddish from a config entry."""
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload a config entry."""
     return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
