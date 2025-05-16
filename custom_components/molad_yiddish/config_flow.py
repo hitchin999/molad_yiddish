a6a777b179beb77bf3305ae5be724ab536d65b73
hitchin999 <hitchin999@gmail.com>
2025-05-16 19:08:29 -0400

    Update config_flow.py

--- custom_components/molad_yiddish/config_flow.py
+++ custom_components/molad_yiddish/config_flow.py
@@ -1,15 +1,19 @@
 """Config flow for Molad Yiddish."""
 import voluptuous as vol
 from homeassistant import config_entries
+
 from .const import DOMAIN
 
 STEP_USER_DATA_SCHEMA = vol.Schema({})
 
 class MoladYiddishConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Molad Yiddish."""
+
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
+        """Handle the initial step (nothing to configure)."""
         if user_input is None:
             return self.async_show_form(step_id="user", data_schema=STEP_USER_DATA_SCHEMA)
+
         return self.async_create_entry(title="Molad (ייִדיש)", data={})
