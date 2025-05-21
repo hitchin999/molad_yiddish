3645bfb53a4d8001a2a8d3d4cbcdb238493b8ab3
hitchin999 <hitchin999@gmail.com>
2025-05-21 17:29:13 -0400

    Update __init__.py

--- custom_components/molad_yiddish/__init__.py
+++ custom_components/molad_yiddish/__init__.py
@@ -1,3 +1,5 @@
+# /homeassistant/custom_components/molad_yiddish/__init__.py
+"""Molad Yiddish integration."""
 """Molad Yiddish integration."""
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
@@ -7,10 +9,20 @@ from .const import DOMAIN
 
 PLATFORMS = [Platform.SENSOR]
 
+
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Molad Yiddish from a config entry."""
+    # Reload integration automatically when options change
+    entry.add_update_listener(_async_update_options)
+
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
+
+
+async def _async_update_options(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Called when config entry options are updated."""
+    await hass.config_entries.async_reload(entry.entry_id)
+
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
